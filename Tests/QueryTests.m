ae7a3c88d67f32cc363f225dfb1f84fb7749263d
Paul Straw <paulstraw@paulstraw.com>
2016-01-28 17:25:25 -0800

    Fixes URL signing + adds more tests. Closes #5

--- Tests/QueryTests.m
+++ Tests/QueryTests.m
@@ -26,12 +26,4 @@
 	XCTAssertEqualObjects(@"bri=50&con=30&exp=20", [self.client URLWithPath:@"sam.jpg"].query);
 }
 
-- (void)testSignedQueryStringCombining {
-	IGXClient *client = [[IGXClient alloc] initWithHost:@"nothingmagical.imgix.net" token:@"sMTidnYXXg9NB4qh"];
-	XCTAssertEqualObjects(@"s=6925569b28658d7a7299f06d520a22dd", [client URLWithPath:@"sam.jpg"].query);
-
-	client.brightness = 50;
-	XCTAssertEqualObjects(@"bri=50&s=75f74662de00a39eb1ac9bd3bd25adb9", [client URLWithPath:@"sam.jpg"].query);
-}
-
 @end
